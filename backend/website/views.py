84240cc231c31d7f86c1d3130bd00c01cb98a886
KvetoslavPrikryl <kvetoslav.prikryl1991@gmail.com>
2023-06-22 19:53:52 +0200

    first commit

---
+++ backend/website/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login, logout
+from django.contrib import messages
+
+def home(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Jsi přihlášen {username}")
+            return redirect("home")
+        else :
+            messages.success(request, "Nesprávné jméno nebo heslo.")
+            return redirect("home")
+    else: 
+        return render(request, "home.html", {})
+
+def login(request):
+    return render(request)
+
+def logout(request):
+    return render(request)
