7bc641062afe02c9448c08fdb4785b5229fc3c0b
Prikryl <uidn4021@contiwan.com>
2023-06-29 11:57:07 +0200

    create user and show on website

--- backend/website/models.py
+++ backend/website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    body = models.TextField(blank=True, null=True)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+
+    def __str__(self):
+        return f"{self.name} {self.body} {self.image}"
--- backend/website/views.py
+++ backend/website/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login, logout
 from django.contrib import messages
+from .models import User
 
 def home(request):
+    user_list = User.objects.all()
+
 
     if request.method == "POST":
+        
         username = request.POST["username"]
         password = request.POST["password"]
 
@@ -17,7 +21,7 @@ def home(request):
             messages.success(request, "Nesprávné jméno nebo heslo.")
             return redirect("home")
     else: 
-        return render(request, "home.html", {})
+        return render(request, "home.html", {"user_list" : user_list})
 
 def logout_user(request):
     logout(request)
