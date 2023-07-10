ab92a3a0572ae3838eaa94df43e260bce62344e3
Prikryl <uidn4021@contiwan.com>
2023-07-10 15:00:19 +0200

    repair styles on website: navbar, logo, kennel and service

--- backend/static/Scripts/base.js
+++ backend/static/Scripts/base.js
@@ -1,12 +1,6 @@
 function hamburger(){
     $("#hamburger").click(function(){
-        if ($(this).childern("nav")){
-            $(".nav").addClass("active").removeClass("nav")
-        }
-        else if ($(this).childern("active")){
-            $(".active").addClass("nav").removeClass("active")
-        }
-        
+        $(".nav").addClass("active").removeClass("nav")
     })
 };
 
